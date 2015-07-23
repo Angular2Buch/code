abe2f447c2bb92d894fb1ae789b3f02dcc02b072
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-07-23 23:05:07 +0200

    first e2e test for hello-world example (which is coupled to gh-pages)

--- e2e/demo.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Protractor Demo Spec', function() {
-
-    it('should have a title', function() {
-        browser.get('http://johanneshoppe.github.io/Presentations2015/Tests-Karma-Protractor/examples/angular_calc/');
-
-        expect(browser.getTitle()).toEqual('AngularJS Calculator');
-
-    });
-});
--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,4 +1,9 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['*.spec.js']
+    specs: ['*.spec.js'],
+
+    // TODO: this is only a temporary solution
+    onPrepare: function() {
+        browser.ignoreSynchronization = true;
+    }
 };
